53036e255ce87dd87eec5bc9f950d7c7e5ab525f
Myles Maxfield <litherum@gmail.com>
2016-08-20 17:11:32 -0700

    IOSurface

--- OffscreenOpenGL/main.swift
+++ OffscreenOpenGL/main.swift
@@ -9,6 +9,8 @@
 import Foundation
 import OpenGL
 import OpenGL.GL3
+import IOSurface
+import CoreVideo
 
 var error = kCGLNoError
 
@@ -127,11 +129,11 @@ printGLError()
 glFramebufferRenderbuffer(GLenum(GL_FRAMEBUFFER), GLenum(GL_COLOR_ATTACHMENT2), GLenum(GL_RENDERBUFFER), renderbuffer)
 printGLError()
 
-let drawBuffers = [ GLenum(GL_NONE), GLenum(GL_COLOR_ATTACHMENT2) ]
+var drawBuffers = [ GLenum(GL_NONE), GLenum(GL_COLOR_ATTACHMENT2) ]
 glDrawBuffers(2, drawBuffers);
 printGLError()
 
-let framebufferStatus = glCheckFramebufferStatus(GLenum(GL_FRAMEBUFFER))
+var framebufferStatus = glCheckFramebufferStatus(GLenum(GL_FRAMEBUFFER))
 switch framebufferStatus {
 case GLenum(GL_FRAMEBUFFER_COMPLETE):
     print("Framebuffer complete")
@@ -155,7 +157,7 @@ default:
     print("Unknown framebuffer status")
 }
 
-let clearColor = [ GLfloat(1), GLfloat(0), GLfloat(0), GLfloat(1) ]
+var clearColor = [ GLfloat(1), GLfloat(0), GLfloat(0), GLfloat(1) ]
 glClearBufferfv(GLenum(GL_COLOR), 1, clearColor)
 printGLError()
 
@@ -169,4 +171,83 @@ print("Color data: \(pixelData)")
 glDeleteRenderbuffers(1, &renderbuffer)
 printGLError()
 glDeleteFramebuffers(1, &framebuffer)
+printGLError()
+
+//CGLTexImageIOSurface2D(ctx: CGLContextObj, _ target: GLenum, _ internal_format: GLenum, _ width: GLsizei, _ height: GLsizei, _ format: GLenum, _ type: GLenum, _ ioSurface: IOSurface, _ plane: GLuint) -> CGLError
+var ioSurfaceBytesPerRow = IOSurfaceAlignProperty(kIOSurfaceBytesPerRow, 16)
+let properties = [ kIOSurfaceWidth as String : 1, kIOSurfaceHeight as String : 1, kIOSurfacePixelFormat as String : Int(kCVPixelFormatType_32BGRA), kIOSurfaceBytesPerRow as String: ioSurfaceBytesPerRow, kIOSurfaceBytesPerElement as String: 16]
+let ioSurface = IOSurfaceCreate(properties)!
+let ioSurfacePixelFormat = IOSurfaceGetPixelFormat(ioSurface)
+let ioSurfaceAllocSize = IOSurfaceGetAllocSize(ioSurface)
+let ioSurfaceBytesPerElement = IOSurfaceGetBytesPerElement(ioSurface)
+ioSurfaceBytesPerRow = IOSurfaceGetBytesPerRow(ioSurface)
+let ioSurfacePlaneCount = IOSurfaceGetPlaneCount(ioSurface)
+print("Usage count: \(IOSurfaceIsInUse(ioSurface))")
+print("IOSurface pixel format: \(ioSurfacePixelFormat) \(kCVPixelFormatType_32RGBA)")
+print("IOSurface alloc size: \(ioSurfaceAllocSize)")
+print("IOSurface bytes per element: \(ioSurfaceBytesPerElement)")
+print("IOSurface bytes per row: \(ioSurfaceBytesPerRow)")
+print("IOSurface plane count: \(ioSurfacePlaneCount)")
+
+var texture = GLuint(0)
+glGenTextures(1, &texture)
+printGLError()
+glBindTexture(GLenum(GL_TEXTURE_RECTANGLE), texture)
+printGLError()
+glTexParameteri(GLenum(GL_TEXTURE_RECTANGLE), GLenum(GL_TEXTURE_BASE_LEVEL), 0);
+printGLError()
+glTexParameteri(GLenum(GL_TEXTURE_RECTANGLE), GLenum(GL_TEXTURE_MAX_LEVEL), 0);
+printGLError()
+error = CGLTexImageIOSurface2D(context, GLenum(GL_TEXTURE_RECTANGLE), GLenum(GL_RGBA), 1, 1, GLenum(GL_BGRA), GLenum(GL_UNSIGNED_INT_8_8_8_8_REV), ioSurface, 0)
+assert(error == kCGLNoError)
+
+glGenFramebuffers(1, &framebuffer)
+printGLError()
+glBindFramebuffer(GLenum(GL_FRAMEBUFFER), framebuffer)
+printGLError()
+glFramebufferTexture2D(GLenum(GL_FRAMEBUFFER), GLenum(GL_COLOR_ATTACHMENT0), GLenum(GL_TEXTURE_RECTANGLE), texture, 0)
+printGLError()
+
+drawBuffers = [ GLenum(GL_COLOR_ATTACHMENT0) ]
+glDrawBuffers(1, drawBuffers);
+printGLError()
+
+framebufferStatus = glCheckFramebufferStatus(GLenum(GL_FRAMEBUFFER))
+switch framebufferStatus {
+case GLenum(GL_FRAMEBUFFER_COMPLETE):
+    print("Framebuffer complete")
+case GLenum(GL_FRAMEBUFFER_UNDEFINED):
+    print("Framebuffer undefined")
+case GLenum(GL_FRAMEBUFFER_INCOMPLETE_ATTACHMENT):
+    print("Framebuffer incomplete attachment")
+case GLenum(GL_FRAMEBUFFER_INCOMPLETE_MISSING_ATTACHMENT):
+    print("Framebuffer incomplete missing attachment")
+case GLenum(GL_FRAMEBUFFER_INCOMPLETE_DRAW_BUFFER):
+    print("Framebuffer incomplete draw buffer")
+case GLenum(GL_FRAMEBUFFER_INCOMPLETE_READ_BUFFER):
+    print("Framebuffer incomplete read buffer")
+case GLenum(GL_FRAMEBUFFER_UNSUPPORTED):
+    print("Framebuffer unsupported")
+case GLenum(GL_FRAMEBUFFER_INCOMPLETE_MULTISAMPLE):
+    print("Framebuffer incomplete multisample")
+case GLenum(GL_FRAMEBUFFER_INCOMPLETE_LAYER_TARGETS):
+    print("Framebuffer incomplete layer targets")
+default:
+    print("Unknown framebuffer status")
+}
+
+clearColor = [ GLfloat(0.1), GLfloat(0.5), GLfloat(0), GLfloat(1) ]
+glClearBufferfv(GLenum(GL_COLOR), 0, clearColor)
+printGLError()
+
+glReadBuffer(GLenum(GL_COLOR_ATTACHMENT0))
+printGLError()
+pixelData = [GLfloat](count: 4, repeatedValue: 0)
+glReadPixels(0, 0, 1, 1, GLenum(GL_RGBA), GLenum(GL_FLOAT), &pixelData)
+printGLError()
+print("Color data: \(pixelData)")
+
+glDeleteFramebuffers(1, &framebuffer)
+printGLError()
+glDeleteTextures(1, &texture)
 printGLError()
